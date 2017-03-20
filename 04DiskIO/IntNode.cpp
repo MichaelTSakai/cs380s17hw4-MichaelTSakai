a07e38ab5fa765461bb3f1e0b302b752ac455bb4
MichaelTSakai <michaeltsakai@gmail.com>
2017-03-19 21:47:18 -0700

    Implemented some functions

---
+++ 04DiskIO/IntNode.cpp
@@ -0,0 +1,40 @@
+#include "IntNode.h"
+
+
+IntNode::~IntNode() 
+{
+}
+
+bool IntNode::operator<(ComparableItem const &right) const
+{
+ 
+}
+
+bool IntNode::operator>(ComparableItem const &right) const
+{
+
+}
+bool IntNode::operator== (ComparableItem const  &right) const
+{
+
+}
+
+void IntNode::changeKey(void*)
+{
+
+}
+
+void IntNode::outputNode(std::ostream& os) const
+{
+
+}
+
+bool IntNode::getKey(void *, int) const
+{
+
+}
+
+bool IntNode::updateKey(void *, int) 
+{
+
+}
--- 04DiskIO/IntNode.h
+++ 04DiskIO/IntNode.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "HNode.h"
+
+class IntNode : public HNode
+{
+public:
+  bool operator< (ComparableItem const &right) const;
+  bool operator> (ComparableItem  const &right) const;
+  bool operator== (ComparableItem const  &right)const;
+
+  void changeKey(void*);
+
+  void outputNode(std::ostream& os) const;
+  ~IntNode();
+
+  bool getKey(void*, int) const;
+
+  bool updateKey(void*, int);
+private: 
+  int mValue;
+};
